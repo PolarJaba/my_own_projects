68973c382fca502c198e2021571b6c73b7eebaf3
PolarJaba <berttalise@gmail.com>
2023-09-20 19:48:11 +0300

    fixed climate

--- climatology/main.py
+++ climatology/main.py
@@ -1,13 +1,15 @@
 # climate row is from jan 1892 to aug 2023
 # 12 months and annual values
-import os
 
 # pip install openpyxl
 # pip install XlsxWriter
 
 import pandas as pd
+import os
 import numpy as np
-from pandas import ExcelWriter
+from statsmodels.tsa.stattools import acf
+from statsmodels.graphics import tsaplots
+import matplotlib.pyplot as plt
 
 # years row creating -> dataframe
 years_list = list(range(1892, 2023 + 1))
@@ -18,29 +20,50 @@ months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 
 values = pd.read_csv('saloniky_climate_rows.txt', skiprows=1, delimiter="\t", names=months)
 values.insert(0, 'year', years['year'])
 values = values.replace([999.9], None)
-print(values)
 
 n = len(years_list)
-statistic_dict = {}
-
+lst = list(range(0, n))
+cell = 1
+plt.figure(figsize=(9, 12))
 for month in months:
     cur_df = values[['year', month]]
-
-    # statistic
-    avg = cur_df[month].sum() / n
-    max_value = cur_df[month].max()
-    min_value = cur_df[month].min()
-    std_temp = cur_df[month].std()
     nulls_count = cur_df[month].isnull().sum().sum()
     nulls_percentage = nulls_count / n * 100
 
-    # None values recovery
     if nulls_percentage <= 10:
+        # statistic
+        avg = cur_df[month].sum() / n
+        asymm = cur_df[month].skew()
 
+        # None values recovery
         cur_df = cur_df.fillna(avg)
-        dikson_row = cur_df.sort_values(by=month)
-        print(dikson_row.head())
-        print(dikson_row.tail())
+
+        # more statistic
+        max_value = cur_df[month].max()
+        min_value = cur_df[month].min()
+        std_temp = cur_df[month].std()
+        cv = std_temp / avg
+        ac = acf(cur_df[month])[1]
+
+        # plot autocorrelation function
+        # fig = tsaplots.plot_acf(cur_df[month], lags=10)
+        # plt.show()
+
+        dikson_row = cur_df.sort_values(by=month).copy()
+
+        if month != 'all_year_val':
+            plt.subplot(4, 3, cell)
+            plt.plot(lst, dikson_row[month], 'ro')
+            plt.xticks([])
+            plt.title(month)
+            cell += 1
+        else:
+            pass
+            """plt.subplot(5, 1, 5)
+            plt.plot(years_list, dikson_row[month], 'ro')
+            plt.xticks([])
+            plt.title(month)
+            cell += 1"""
         # dikson criteria
         d1_max = round((dikson_row.at[n - 1, month] - dikson_row.at[n - 2, month]) / (
                 dikson_row.at[n - 1, month] - dikson_row.at[0, month]), 2)
@@ -79,6 +102,9 @@ for month in months:
                                      ["Min", min_value, None, None],
                                      ["Average", round(avg, 1), None, None],
                                      ["STD", round(std_temp, 1), None, None],
+                                     ["Variation coefficient", round(cv, 2), None, None],
+                                     ["Asymmetry coefficient", round(asymm, 2), None, None],
+                                     ["Autocorrelation coefficient", round(ac, 5), None, None],
                                      ["Dixon", None, None, None],
                                      ["TO MAX", None, "TO MIN", None],
                                      ["D1", d1_max, "D1", d1_min],
@@ -98,3 +124,5 @@ for month in months:
         print(f"Большое количество пропусков: {round(nulls_percentage, 2)}% в ряду {month}, восстановите значения, либо"
               f" измените порог nulls_percentage")
 
+plt.subplots_adjust(hspace=0.20, wspace=0.25)
+plt.savefig('all_months.png', dpi=300)
